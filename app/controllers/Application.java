43fa1f7ee2142d483d72715ebd2ec198bd4627af
Rudolf Frederiksen <rudolf@webube.com>
2012-03-05 15:05:02 +0100

    Add a mobile device detection and redirection for iOS/Android/BB.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,12 +3,26 @@ package controllers;
 import play.*;
 import play.mvc.*;
 
+import java.util.ArrayList;
 
 
 public class Application extends Controller {
 
+    private static  void checkMobile () {
+        String              user_agent = request.headers.get("user-agent").value();
+        String[]            keyWords = {"iPad", "iPhone", "Android", "BlackBerry"};
+        int                 mobileAgent = -1;
+        
+        for (int i = 0; i < keyWords.length && mobileAgent < 0; i++)
+            mobileAgent = user_agent.indexOf(keyWords[i]);
+
+        if (mobileAgent > 0)
+            redirect("/public/appocalypse-demo.html");
+    }
+
     public static void index()
     {
+        Application.checkMobile();
         render();
     }
 
