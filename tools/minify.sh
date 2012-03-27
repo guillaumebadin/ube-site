2aa3de6d6f13f649219a81e6238d1d28f0bbe43b
Rudolf Frederiksen <rudolf@webube.com>
2012-03-27 15:40:55 +0200

    Moved all images from RackSpace US to RackSpace UK.

--- tools/minify.sh
+++ tools/minify.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-version="v9";
+version="v10";
 lessc='bin/less.js/bin/lessc';
 compressor="/usr/bin/java -jar bin/compiler.jar";
 yuicompressor="/usr/bin/java -jar bin/yuicompressor-2.4.7.jar"
