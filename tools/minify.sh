7f81716faf46b48502becfeb12e47b302aec86d5
Guillaume Badin <guillaumebadin@webube.com>
2012-04-14 10:06:24 +0200

    fix css

--- tools/minify.sh
+++ tools/minify.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-version="v10";
+version="v11";
 lessc='bin/less.js/bin/lessc';
 compressor="/usr/bin/java -jar bin/compiler.jar";
 yuicompressor="/usr/bin/java -jar bin/yuicompressor-2.4.7.jar"
